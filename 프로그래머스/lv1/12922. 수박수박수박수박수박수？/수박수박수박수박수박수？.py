5344f5433c8b33eccff3f3328ee613f22b143187
yoon gyu heon <121912366+ykh9871@users.noreply.github.com>
2023-01-23 23:24:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,8 +1,4 @@
 def solution(n):
     answer = ''
-    for i in range(n):
-        if i % 2 == 0:
-            answer += '수'
-        else:
-            answer += '박'
-    return answer
+    s = "수박" * n
+    return s[:n]
