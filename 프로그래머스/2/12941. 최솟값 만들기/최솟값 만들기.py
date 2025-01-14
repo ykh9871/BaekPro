ed418e80ea9bc489157ef58bed1393fe59488a98
썩지않는사과 <121912366+ykh9871@users.noreply.github.com>
2025-01-15 04:08:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.46 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,7 +1,2 @@
-def solution(A,B):
-    A.sort()
-    B.sort(reverse=True)
-    answer = 0
-    for i in range(len(A)):
-        answer += A[i] * B[i]
-    return answer
+def solution(A, B):
+    return sum(a * b for a, b in zip(sorted(A), sorted(B, reverse=True)))
